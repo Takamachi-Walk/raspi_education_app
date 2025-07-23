e4a273022c0316fb1048dea7666a12ec29503555
Takamachi-Walk <toma.sugawara@gr.tsubakimoto.co.jp>
2025-07-23 14:00:23 +0900

    ラズパイ環境で開発 ・キーが効かない問題 ・デバッグウィンドウ実装&表示切替 ・一連の軽い動作確認〇

--- raspi_education_app/start_app.sh
+++ raspi_education_app/start_app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Flaskアプリ起動（バックグラウンド）
-python3 /home/monokai/Desktop/video_preload_app/app.py &
+python3 /home/monokai/Desktop/git/raspi_education_app/app.py &
 
 # 少し待機してからブラウザ起動
 sleep 3
 
 # Chromiumをフルスクリーン(kiosk)で起動
-chromium-browser --kiosk http://localhost:8080
+firefox --kiosk http://localhost:8080
